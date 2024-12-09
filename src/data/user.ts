cb1a0c599f02a2132382fbe11045c59dc16453d1
paulogilvan <paulogilvan74@gmail.com>
2024-12-08 21:46:47 -0300

    criação page home, componente nav-logout, nav-profile, nav-item, pasta data e types

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'gilvan',
+    name: 'Paulo Gilvan',
+    avatar: 'https://ui-avatars.com/api/?background=random&name=Paulo&size=100',
+    cover: 'https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg',
+    bio: 'Desenvolvedor Full Stack, apaixonado por tecnologia e inovação',
+    link: 'https://gilvanweb.com.br',
+    postCount: 208
+}
